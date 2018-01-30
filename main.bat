2a16e62e3665d22c7a541a607239f6abdad35c3e
RR-Records <ethan.o2k17@icloud.com>
2018-01-30 01:20:48 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,7 +1,29 @@
 @echo off & setlocal
 color fc
+set momentum=0.9
+set learningrate=0.01
 goto Main
 
+###
+
 :Main
-cls
-set method=(%random%*10/32768)+1
+set /a int=%random%*1/32768+1-1
+
+###
+
+:Neuron
+###Sets gain with a weight of 1-3###
+set /a gain=%random%*3/32768+1
+###creates the neurons input-later to be direct file input###
+set /a dend0=%random%*2/32768+1-1
+set /a dend0=%dend0%/%gain%
+
+set /a dend1=%random%*2/32768+1-1
+set /a dend1=%dend1%/%gain%
+ 
+set /a dend2=%random%*2/32768+1-1
+set /a dend2=%dend2%/%gain%
+
+###output
+set /a output=%dend0%=%dend1%+%dend2%/3
+echo %output%
