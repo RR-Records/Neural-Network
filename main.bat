cf9bdcafcd7962d17682d9164a6fd0e1c25e56aa
RR-Records <ethan.o2k17@icloud.com>
2018-02-05 11:33:40 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -4,17 +4,20 @@ set momentum=0.9
 set learningrate=0.01
 goto Main
 
-###
+###main
 
-:Main
-set /a int=%random%*1/32768+1-1
+:Main 
+set /a int = %random% * 1 / 32768 + 1 - 1
 
-###
+###neuron
 
 :Neuron
 ###Sets gain with a weight of 1-3###
-set /a gain=%random%*3/32768+1
+
+set /a gain= %random% * 3 /32768 + 1
+
 ###creates the neurons input-later to be direct file input###
+
 set /a dend0=%random%*2/32768+1-1
 set /a dend0=%dend0%/%gain%
 
